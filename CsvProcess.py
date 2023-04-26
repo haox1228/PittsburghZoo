57fe5284b26a1003443248c5b688e66d848c6028
马浩轩 <mahaoxuan@s-macbook-pro-4.mynetworksettings.com>
2023-04-25 22:57:35 -0400

    add comments and the graph feature

--- CsvProcess.py
+++ CsvProcess.py
@@ -2,16 +2,59 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Apr 25 12:51:12 2023
+The script that used to process the csv data
 @author: Nick, Samson, Masu (Team: Cold Brew)
 """
 
 import pandas as pd
 from fuzzywuzzy import process
 
+# read the csv
 animal_type = pd.read_csv('class.csv')
 zoo = pd.read_csv('zoo.csv')
+import matplotlib.pyplot as plt
+
+
+def show_stats(filtered_df):
+    '''
+    This method creates three graph for users to get basic infomation of the animals in the zoo
+    :param filtered_df: the df that has been filtered with only animals exist in the pgh zoo
+    :return: graphs and a sentence
+    '''
+    # pie chart for animal count by type
+    animal_count = filtered_df['type'].value_counts()
+    animal_count.plot(kind='pie', autopct='%1.1f%%', startangle=90)
+    plt.title("Animal Count by Type (Pie chart)")
+    plt.ylabel("")
+    plt.show()
+
+    # bar chart for animal count by type
+    animal_count.plot(kind='bar')
+    plt.title("Animal Count by Type (Bar chart)")
+    plt.xlabel("Animal Type")
+    plt.ylabel("Count")
+    plt.show()
+
+    # box plot for animal count by legs and type
+    fig, ax = plt.subplots()
+    filtered_df.boxplot(column='legs', by='type', ax=ax)
+    plt.title("Animal Count by Legs and Type (Box plot)")
+    plt.suptitle("")
+    plt.xlabel("Animal Type")
+    plt.ylabel("Legs")
+    plt.show()
+
+    print('Your graph has been generated')
+
 
 def cleanData(animal_type, zoo):
+    '''
+    This is the method that used to clean the csv data, it will join
+    two csvs together and then drop a few columns
+    :param animal_type: a csv
+    :param zoo: another csv
+    :return: a cleaned up csv
+    '''
     animal_type = animal_type.drop(['Number_Of_Animal_Species_In_Class', 'Animal_Names'], axis=1)
     zoo = pd.merge(zoo, animal_type, left_on=('class_type'), right_on=('Class_Number'))
     zoo['animal_name'] = zoo['animal_name'].str.capitalize()
@@ -20,16 +63,25 @@ def cleanData(animal_type, zoo):
     return zoo
 
 
-def matchAnimal(df, search):
+def matchAnimal(df, search, print_sentences=True):
+    '''
+    This method will use the search name and the csv file, to search if the
+    animal exists in the df, and used a fuzzy search since there won't always be an
+    exact match. And then group out features to make a grammarly logical sentense
+    to prompt to the user
+    :param df: the csv
+    :param search: the search string
+    :return: return a matched animal from fuzzy search for making the api call
+    and a string sentence to prompt to the user.
+    '''
     animals = df['animal_name'].tolist()
     best_match = process.extractOne(search, animals)
 
     if best_match[1] >= 80:
         matched_animal = best_match[0]
         matched_row = df[df['animal_name'] == matched_animal].iloc[0]
-
-        print(f"{search} seems a kind of '{matched_animal}'.")  # Added line
-
+        if print_sentences:
+            print(f"{search} seems a kind of '{matched_animal}'.")  # Added line
 
         group1 = [col for col in ['hair', 'feathers', 'toothed', 'backbone', 'fins', 'legs', 'tail'] if
                   matched_row[col] == 1]
@@ -37,7 +89,7 @@ def matchAnimal(df, search):
         group3 = [col for col in ['venomous', 'predator'] if matched_row[col] == 1]
         group4 = [col for col in ['domestic'] if matched_row[col] == 1]
 
-        # Generate a sentence
+        # generate a sentence
         sentence = f"{matched_animal} is a "
 
         if group4:
@@ -56,4 +108,3 @@ def matchAnimal(df, search):
         return matched_animal, sentence
     else:
         return '', "No matching animal found, but try your luck at getting detail information feature"
-
--- main.py
+++ main.py
@@ -5,64 +5,66 @@ Created on Tue Apr 25 12:51:12 2023
 @author: Nick, Samson, Masu (Team: Cold Brew)
 """
 import pandas as pd
-from CsvProcess import cleanData, matchAnimal
+from CsvProcess import cleanData, matchAnimal, show_stats
 from webcrawler import crawler
 from APICall import call_api, wording
 
 def main():
+    '''
+    The main routine of the program, have include all the use cases in the proposal
+    '''
     # Load and clean the data
     animal_type = pd.read_csv('class.csv')
     zoo = pd.read_csv('zoo.csv')
     df = cleanData(animal_type, zoo)
 
     animal_dict = crawler()
-
     # List of animal options
     # animal_options = ['Canadian Lynx', 'Giant Panda', 'Komodo Dragon', 'Tiger']
     animal_areas = list(animal_dict.keys())
 
     while True:
-        # User prompt
-        print("\nWhich area do you want to know? (Press 0 to exit)")
-        for i, option in enumerate(animal_areas, 1):
-            print(f"{i}. {option}")
+        # first layer of ui
+        print("\nHello there, this is the PGH Zoo Virtual Tour guide, how can i help you?")
+        print("1. View statistical data of the PGH zoo")
+        print("2. Know the animals in the PGH zoo")
+        print("0. Exit")
 
-        # Get user input for animal area
         choice = int(input("> "))
 
-        # Exit when user inputs 0
         if choice == 0:
             break
+        # if user choose 1, then generate graph
+        if choice == 1:
+            # filter out the csv data with only animals in the pgh zoo
+            matched_animals = []
+            for area, animals in animal_dict.items():
+                for animal in animals:
+                    matched_animal, _ = matchAnimal(df, animal,False)
+                    if matched_animal:
+                        matched_animals.append(matched_animal)
 
-        # Ensure valid input
-        if 1 <= choice <= len(animal_areas):
-            print('\nWhich animal do you want to know? (Press 0 to exit)')
-            animal_options = list(animal_dict.get(animal_areas[choice-1]))
-            for i, option in enumerate(animal_options, 1):
+            matched_animals = list(set(matched_animals))
+            filtered_df = df[df['animal_name'].isin(matched_animals)]
+            #show the graphs
+            show_stats(filtered_df)
+        elif choice == 2:
+            # User prompt
+            print("\nWhich area do you want to know? (Press 0 to exit)")
+            for i, option in enumerate(animal_areas, 1):
                 print(f"{i}. {option}")
 
-            # Get user input for specific animal
+            # Get user input for animal area
             choice = int(input("> "))
 
             # Exit when user inputs 0
             if choice == 0:
                 break
 
-            search_string = animal_options[choice - 1]
-            matched_animal, result = matchAnimal(df, search_string)
-            print(result)
-
-            # in case of not existing in csv but can find from API such as panda
-            # extract a last word from animal name (can cover mostly)
-            if matched_animal == '':
-                matched_animal = search_string.split()[-1]
-
-            animals_apiinfo = call_api(matched_animal)
-
-            if len(animals_apiinfo) > 0:
-                print('I can provide detailed information for the below species. (select one of enter 0 to exit)')
-
-                animal_options = [d.get('name') for d in animals_apiinfo]
+            # Ensure valid input
+            if 1 <= choice <= len(animal_areas):
+                print('\nWhich animal do you want to know? (Press 0 to exit)')
+                animal_options = list(animal_dict.get(animal_areas[choice-1]))
                 for i, option in enumerate(animal_options, 1):
                     print(f"{i}. {option}")
 
@@ -73,15 +75,40 @@ def main():
                 if choice == 0:
                     break
 
-                # Choose one animal dict and wording information
-                target_animal_dict = animals_apiinfo[choice - 1]
-                explanation = wording(target_animal_dict)
+                search_string = animal_options[choice - 1]
+                matched_animal, result = matchAnimal(df, search_string)
+                print(result)
 
-                # Print detailed animal info
-                print(explanation)
+                # in case of not existing in csv but can find from API such as panda
+                # extract a last word from animal name (can cover mostly)
+                if matched_animal == '':
+                    matched_animal = search_string.split()[-1]
 
-            else:
-                print("Sorry, I can't find this animal's information")
+                animals_apiinfo = call_api(matched_animal)
+
+                if len(animals_apiinfo) > 0:
+                    print('I can provide detailed information for the below species. (select one of enter 0 to exit)')
+
+                    animal_options = [d.get('name') for d in animals_apiinfo]
+                    for i, option in enumerate(animal_options, 1):
+                        print(f"{i}. {option}")
+
+                    # Get user input for specific animal
+                    choice = int(input("> "))
+
+                    # Exit when user inputs 0
+                    if choice == 0:
+                        break
+
+                    # Choose one animal dict and wording information
+                    target_animal_dict = animals_apiinfo[choice - 1]
+                    explanation = wording(target_animal_dict)
+
+                    # Print detailed animal info
+                    print(explanation)
+
+                else:
+                    print("Sorry, I can't find this animal's information")
         else:
             print("Invalid input.")
 
